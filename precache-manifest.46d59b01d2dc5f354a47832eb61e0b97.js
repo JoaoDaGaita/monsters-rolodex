aaa0f4bea85617d1749f5993fed2bf3991fe7c0e
joaozinho22 <joao.silva.008@acad.pucrs.br>
2020-08-26 16:24:06 -0300

    Updates

---
+++ precache-manifest.46d59b01d2dc5f354a47832eb61e0b97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb3c335d203820be4620533ef7d8199",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "36bda41261116b97de5f",
+    "url": "/monsters-rolodex/static/css/main.f219193e.chunk.css"
+  },
+  {
+    "revision": "187589cf30d67f968547",
+    "url": "/monsters-rolodex/static/js/2.a86903fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a86903fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36bda41261116b97de5f",
+    "url": "/monsters-rolodex/static/js/main.4a6c9ec4.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
